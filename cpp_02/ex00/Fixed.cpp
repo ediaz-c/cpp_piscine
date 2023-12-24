0e759ed5ab4c9f9f0c75d2910ee2e06ead9f5d7d
ediaz-c <centeno222@live.com>
2023-12-24 17:11:42 +0100

    cpp02

--- cpp_02/ex00/Fixed.cpp
+++ cpp_02/ex00/Fixed.cpp
@@ -1,50 +1,44 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Fixed.cpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: erick <erick@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 19:01:32 by erick             #+#    #+#             */
-/*   Updated: 2023/11/07 19:16:57 by erick            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Fixed.hpp"
 
 const int Fixed::_bitsFract = 8;
 
+// Constructor por defecto
 Fixed::Fixed(void)
 {
-	std::cout<< "Default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 	this->setRawBits(0);
 }
 
+// Destructor
 Fixed::~Fixed(void)
 {
 	std::cout << "Destructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &cpy)
+// Constructor de copia
+Fixed::Fixed(Fixed const &cpy)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = cpy;
 }
 
-Fixed & Fixed::operator=(const Fixed &equal)
+Fixed &Fixed::operator=(Fixed const &equal)
 {
 	std::cout << "Assignation operator called" << std::endl;
-	this->_valueFixed = equal.getRawBits();
+	if (this != &equal)
+	{
+		this->_valueFixed = equal.getRawBits();
+	}
 	return (*this);
 }
 
-int	Fixed::getRawBits(void) const
+int Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_valueFixed >> Fixed::_bitsFract);
 }
 
-void Fixed::setRawBits(const int value)
+void	Fixed::setRawBits(int const value)
 {
 	this->_valueFixed = value << Fixed::_bitsFract;
 }
